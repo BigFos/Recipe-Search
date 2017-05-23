1b6ca88d69177b2906843183b1b79f66ec97b256
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-05-22 21:01:23 -0700

    firebase

---
+++ javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
+    authDomain: "recipe-search-ccf28.firebaseapp.com",
+    databaseURL: "https://recipe-search-ccf28.firebaseio.com",
+    projectId: "recipe-search-ccf28",
+    storageBucket: "recipe-search-ccf28.appspot.com",
+    messagingSenderId: "5834477742"
+  };
+  firebase.initializeApp(config);
