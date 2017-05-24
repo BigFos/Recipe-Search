438264da550a3aa3552abc43d102c49640761a1a
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-05-23 18:14:25 -0700

    maincss

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,17 @@
+$(document).ready(function(){
+
+    $('.dropdown-button').dropdown();
+    
+  });
+
+
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
-    authDomain: "recipe-search-ccf28.firebaseapp.com",
-    databaseURL: "https://recipe-search-ccf28.firebaseio.com",
-    projectId: "recipe-search-ccf28",
-    storageBucket: "recipe-search-ccf28.appspot.com",
-    messagingSenderId: "5834477742"
-  };
-  firebase.initializeApp(config);
+  // var config = {
+  //   apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
+  //   authDomain: "recipe-search-ccf28.firebaseapp.com",
+  //   databaseURL: "https://recipe-search-ccf28.firebaseio.com",
+  //   projectId: "recipe-search-ccf28",
+  //   storageBucket: "recipe-search-ccf28.appspot.com",
+  //   messagingSenderId: "5834477742"
+  // };
+  // firebase.initializeApp(config);
