88e2b3b43149592b3cec2554f34fd3a9378f90b2
Steven Foster Jr <Steven Foster Jr>
2017-05-24 19:08:55 -0700

    updated js

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,24 @@
+$(document).ready(function(){
+
+    $('.dropdown-button').dropdown();
+    
+  });
+
+
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
-    authDomain: "recipe-search-ccf28.firebaseapp.com",
-    databaseURL: "https://recipe-search-ccf28.firebaseio.com",
-    projectId: "recipe-search-ccf28",
-    storageBucket: "recipe-search-ccf28.appspot.com",
-    messagingSenderId: "5834477742"
-  };
-  firebase.initializeApp(config);
+  // var config = {
+  //   apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
+  //   authDomain: "recipe-search-ccf28.firebaseapp.com",
+  //   databaseURL: "https://recipe-search-ccf28.firebaseio.com",
+  //   projectId: "recipe-search-ccf28",
+  //   storageBucket: "recipe-search-ccf28.appspot.com",
+  //   messagingSenderId: "5834477742"
+  // };
+  // firebase.initializeApp(config);
+
+var userSearchR = $("#td1").val().trim();
+var userSearchA = $("#td2").val().trim();
+var queryURLR = "https://api.edamam.com/search?q=" + userSearchR + "&app_id=38c12157&app_key=4a58a48eab717dc057b5c88ba03bb32f";
+// var queryURLA = ""
+console.log(userSearchR);
+console.log(userSearchA);
