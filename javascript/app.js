68148f82c3f54b72edb905b9944030718f92fa5e
Steven Foster Jr <Steven Foster Jr>
2017-05-24 18:45:38 -0700

    updated js

--- javascript/app.js
+++ javascript/app.js
@@ -15,3 +15,8 @@ $(document).ready(function(){
   //   messagingSenderId: "5834477742"
   // };
   // firebase.initializeApp(config);
+
+var userSearchR = $("#").val().trim();
+var userSearchA = $("#").val().trim();
+var queryURLR = "https://api.edamam.com/search?q=" + userSearchR + "&app_id=38c12157&app_key=4a58a48eab717dc057b5c88ba03bb32f";
+var queryURLA = ""
