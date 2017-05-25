763748d90a0b356c2139ea4fa288c9e56edafeef
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-05-24 21:18:14 -0700

    finished on wednesday

--- javascript/app.js
+++ javascript/app.js
@@ -2,6 +2,20 @@ $(document).ready(function() {
 
     $('.dropdown-button').dropdown();
 
+    // Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
+  authDomain: "recipe-search-ccf28.firebaseapp.com",
+  databaseURL: "https://recipe-search-ccf28.firebaseio.com",
+  projectId: "recipe-search-ccf28",
+  storageBucket: "recipe-search-ccf28.appspot.com",
+  messagingSenderId: "5834477742"
+};
+firebase.initializeApp(config);
+
+
+var database=firebase.database();
+
     var ingredientArray = [];
 
  var querySearch = "";
@@ -13,10 +27,19 @@ $(document).ready(function() {
         ingredientArray.push(input);
         console.log($("#ingredientList").children());
         console.log(ingredientArray);
+        $(".close").on("click",function(item){
+        	var slice= ingredientArray.indexOf(item);
+        	$(ingredientArray).splice(slice,);
+        	console.log(ingredientArray);
+        });
+
+
     });
 
     $("#recipeButton").on("click", function() {
     	$("#recipeCards").empty();
+
+
     	            for (i = 0; i < ingredientArray.length; i++) {
 
        querySearch += ingredientArray[i]+ ",+";
@@ -30,14 +53,18 @@ $(document).ready(function() {
 
         }).done(function(response) {
             for (i = 0; i < response.hits.length; i++) {
-                $("#recipeCards").append('<div class="card">' + '<div class= "card-image">' + '<img src="' + response.hits[i].recipe.image + '"></div>' + '<div class="card-content">' + '<p>' + response.hits[i].recipe.label + '</p></div>' + '<div class=card-action>' + '<a href="#">' + "More info" + '</a>' + '<a href="#">' + "Buy Other items on Amazon" + '</a></div></div>');
-
-
-
+                $("#recipeCards").append('<div class="card">' + '<div class= "card-image">' + '<img src="' + response.hits[i].recipe.image + '"></div>' + '<div class="card-content">' + '<p>' + response.hits[i].recipe.label + '</p></div>' + '<div class=card-action>' + '<ul>'+ '<li>'+response.hits[i].recipe.ingredientLines +'</li>'+ '</ul>'+'<a id="list" href="'+response.hits[i].recipe.url+'">' + "Instructions" + '</a>' + '<input id="amazon" >' + "Buy Other items on Amazon" + '</input></div></div>');
+                $("#hide").hide();
             }
+             database.ref().push({
+                	Ingredient: ingredientArray
+                });
         });
         console.log(queryURLR);
 
+    });
+    $("#list").on("click",function(){
+    	$("#hide").show();
     });
 
 });
@@ -45,13 +72,4 @@ $(document).ready(function() {
 
 
 
-Initialize Firebase
-var config = {
-  apiKey: "AIzaSyDPfYzQy3MaKGqQbEUWplRbldAMNMTdIOo",
-  authDomain: "recipe-search-ccf28.firebaseapp.com",
-  databaseURL: "https://recipe-search-ccf28.firebaseio.com",
-  projectId: "recipe-search-ccf28",
-  storageBucket: "recipe-search-ccf28.appspot.com",
-  messagingSenderId: "5834477742"
-};
-firebase.initializeApp(config);
+
